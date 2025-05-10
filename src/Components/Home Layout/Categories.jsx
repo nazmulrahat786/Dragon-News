1e34bec2f64031ed46f59305bc97875995f00095
nazmulrahat786 <nazmulrahat786.13@gmail.com>
2025-05-11 00:13:51 +0600

    aa

--- src/Components/Home Layout/Categories.jsx
+++ src/Components/Home Layout/Categories.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { use } from 'react';
+const catagoryPromis = fetch("/categories.json").then(res => res.json());
 
 const Categories = () => {
+    const catagories = use(catagoryPromis);
     return (
         <div>
-            
+            <h1 className='font-bold'>All Catagories</h1>
         </div>
     );
 };
